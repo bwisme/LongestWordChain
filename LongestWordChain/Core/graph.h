89a2c70ade5c1865005ea747c9a07f9772c9d3ac
bwisme <z@bwis.me>
2019-03-13 00:28:36 +0800

    init core again

---
+++ LongestWordChain/Core/graph.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <cstring>
+#include <vector>
+#define MAX_EDGE 10010
+#define MAX_NODE 36
+
+typedef struct edge
+{
+    int from;
+    int to;
+    int weight;
+    char* word;
+} edge;
+
+
+class graph
+{
+public:
+    graph();
+    ~graph();
+
+
+    int head[MAX_NODE];
+    int next[MAX_EDGE];
+    edge edges[MAX_EDGE];
+
+	int path[MAX_EDGE];
+	int path_len;
+
+    int loop_result[MAX_EDGE]; //有环的情况才会用到 一个全局变量，存储的是当前最长路径（边
+    int loop_ans;//有环的情况才会用到 一个全局变量，存储的是当前最长路径的值
+	int loop_len;
+
+    bool self_loop; //是否有自环
+    //记录的是从每个点出发的所有自环中的边
+    std::vector< std::vector<int> > self_loop_edges;
+    
+    std::vector<int> topo_result;
+
+    int edge_count;
+
+    //void init();
+
+    void add_edges(int from, int to, int w, char* word);
+
+    bool dfs(int u);
+
+    bool topological_sort();
+
+    int id[MAX_NODE];
+
+    bool vis_node[MAX_NODE];
+    
+    bool vis_edge[MAX_EDGE];
+
+    bool vis_self_loop[MAX_NODE];
+
+    int char_to_int(char ch);
+    
+    void search_longest_path(int u, int ans, int tail);
+
+    int force_dfs(char* result[], int head, int tail);
+    
+    bool is_node(int u);
+
+private:
+
+
+    
+
+
+};
+
+
+
