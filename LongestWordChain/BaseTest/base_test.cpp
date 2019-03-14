767e55a771d1cb0134146185b42639dea1aab19f
bwisme <z@bwis.me>
2019-03-14 13:02:58 +0800

    exception test for parse_arguments finished

--- LongestWordChain/BaseTest/base_test.cpp
+++ LongestWordChain/BaseTest/base_test.cpp
@@ -170,9 +170,125 @@ namespace BaseTest
 			}
 			catch (const std::invalid_argument& e)
 			{
-				Assert::AreEqual("parse_arguments: Too much -w appeared", e.what());
+				Assert::AreEqual("parse_arguments: No filename detected", e.what());
 			}
 
+		}
+		TEST_METHOD(ExceptionTest3)
+		{
+			try
+			{
+				int argc = 2;
+				char* argv[] = { "WordList.exe","a.txt" };
+				base b(argc, argv);
+				std::string filename;
+				int mode;
+				char head, tail;
+				bool enable_loop;
+				b.parse_arguments(&filename, &mode, &head, &tail, &enable_loop);
+				Assert::Fail();
+			}
+			catch (const std::invalid_argument& e)
+			{
+				Assert::AreEqual("parse_arguments: No -w and -c appeared", e.what());
+			}
+
+		}
+		TEST_METHOD(ExceptionTest4)
+		{
+			try
+			{
+				int argc = 4;
+				char* argv[] = { "WordList.exe","-w", "-c", "a.txt" };
+				base b(argc, argv);
+				std::string filename;
+				int mode;
+				char head, tail;
+				bool enable_loop;
+				b.parse_arguments(&filename, &mode, &head, &tail, &enable_loop);
+				Assert::Fail();
+			}
+			catch (const std::invalid_argument& e)
+			{
+				Assert::AreEqual("parse_arguments: -w and -c can't be set at the same time", e.what());
+			}
+
+		}
+		TEST_METHOD(ExceptionTest5)
+		{
+			try
+			{
+				int argc = 4;
+				char* argv[] = { "WordList.exe","-c", "-c", "a.txt" };
+				base b(argc, argv);
+				std::string filename;
+				int mode;
+				char head, tail;
+				bool enable_loop;
+				b.parse_arguments(&filename, &mode, &head, &tail, &enable_loop);
+				Assert::Fail();
+			}
+			catch (const std::invalid_argument& e)
+			{
+				Assert::AreEqual("parse_arguments: Too much -c appeared", e.what());
+			}
+		}
+		TEST_METHOD(ExceptionTest6)
+		{
+			try
+			{
+				int argc = 7;
+				char* argv[] = { "WordList.exe","-c", "-h","a","-h","b", "a.txt" };
+				base b(argc, argv);
+				std::string filename;
+				int mode;
+				char head, tail;
+				bool enable_loop;
+				b.parse_arguments(&filename, &mode, &head, &tail, &enable_loop);
+				Assert::Fail();
+			}
+			catch (const std::invalid_argument& e)
+			{
+				Assert::AreEqual("parse_arguments: Too much -h appeared", e.what());
+			}
+		}
+		TEST_METHOD(ExceptionTest7)
+		{
+			try
+			{
+				int argc = 7;
+				char* argv[] = { "WordList.exe","-c", "-t","t","-t","f", "a.txt" };
+				base b(argc, argv);
+				std::string filename;
+				int mode;
+				char head, tail;
+				bool enable_loop;
+				b.parse_arguments(&filename, &mode, &head, &tail, &enable_loop);
+				Assert::Fail();
+			}
+			catch (const std::invalid_argument& e)
+			{
+				Assert::AreEqual("parse_arguments: Too much -t appeared", e.what());
+			}
+		}
+		TEST_METHOD(ExceptionTest8)
+		{
+			try
+			{
+				int argc = 5;
+				char* argv[] = { "WordList.exe","-c", "a.txt", "-r","-r" };
+				base b(argc, argv);
+				std::string filename;
+				int mode;
+				char head, tail;
+				bool enable_loop;
+				b.parse_arguments(&filename, &mode, &head, &tail, &enable_loop);
+				Assert::Fail();
+			}
+			catch (const std::invalid_argument& e)
+			{
+				Assert::AreEqual("parse_arguments: Too much -r appeared", e.what());
+			}
 		}
 
 	
